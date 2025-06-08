1c531dbfc5b6591afe9bab335ad0adbd121ddcdd
Rajpreet0 <rajpreet.singh@gmx.de>
2025-06-08 14:30:30 +0200

    getting currency data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Logo from "../../public/oily_logo.png";
 import TankstellenCard from "@/components/dashboardComponents/TankstellenCard";
+import ExchangeCard from "@/components/dashboardComponents/ExchangeCard";
 
 export default function Home() {
 
@@ -9,6 +10,7 @@ export default function Home() {
     <div className="p-4">
       <Image src={Logo} width={150} height={100} alt="Oily Logo"/>
       <TankstellenCard/>
+      <ExchangeCard/>
     </div>
   );
 }
